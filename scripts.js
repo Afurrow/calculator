0eb57a56bc27052709f4ac70419706dd9fcb55ec
AFurrow <austindfurrow@gmail.com>
2020-08-26 20:31:31 -0600

    Fixed Incorrect input error

--- scripts.js
+++ scripts.js
@@ -1,31 +1,36 @@
 function addToInput(btn) {
     const curBtnClass = btn.classList[0];
-    [lastClass, curClass] = [curClass, curBtnClass]; 
-    const inputBox = document.querySelector(".input");
-    const resultBox = document.querySelector(".result");
+    console.log([curClass, lastClass])
+    if (curClass === curBtnClass && curClass === "numpad" ||
+               (curBtnClass === "numpad" && curClass === "equals")) {
+        alert("Please click an operator");
+    } else if (curClass === undefined && curBtnClass === "operator") {
+        alert("Please click a number")
+    } else {
+        [lastClass, curClass] = [curClass, curBtnClass]; 
+        const inputBox = document.querySelector(".input");
+        const resultBox = document.querySelector(".result");
 
-    if((lastClass === "operator" && curClass === "operator") || 
-       (lastClass === undefined && curClass === "numpad")) {
-        inputBox.textContent = btn.textContent;
-    } else if ((lastClass === "numpad" && curClass === "operator") ||
-               (lastClass === "operator" && curClass === "numpad") ||
-               (lastClass === "equals" && curClass === "operator")) {
-        resultBox.textContent = resultBox.textContent + " " + inputBox.textContent;
-        inputBox.textContent = btn.textContent;
-    } else if ((lastClass === undefined && curClass === "operator")) {
-        alert("Please click a number");
-    } else if (lastClass === "numpad" && curClass === "numpad") {
-        inputBox.textContent = inputBox.textContent + btn.textContent;
-    } else if (lastClass === "equals" && curClass === "numpad") {
-        alert("Please click an operator")
-    }
+        if((lastClass === "operator" && curClass === "operator") || 
+        (lastClass === undefined && curClass === "numpad")) {
+            inputBox.textContent = btn.textContent;
+        } else if ((lastClass === "numpad" && curClass === "operator") ||
+                (lastClass === "operator" && curClass === "numpad") ||
+                (lastClass === "equals" && curClass === "operator")) {
+            lastInsert = inputBox.textContent;
+            resultBox.textContent = resultBox.textContent + " " + inputBox.textContent;
+            inputBox.textContent = btn.textContent;
+        } else if (lastClass === "numpad" && curClass === "numpad") {
+            inputBox.textContent = inputBox.textContent + btn.textContent;
+        } 
 
-    resultArr = document.querySelector(".result")
-                        .textContent.split(" ")
-                        .filter(x => x !== "");
+        resultArr = document.querySelector(".result")
+                            .textContent.split(" ")
+                            .filter(x => x !== "");
 
-    if(resultArr.length === 3) {
-        getResults(false);
+        if(resultArr.length === 3) {
+            getResults(false);
+        }
     }
 }   
 
@@ -33,13 +38,15 @@ function getResults(fromButton = true) {
     const resultsBox = document.querySelector(".result");
     const inputBox = document.querySelector(".input");
     if (curClass === "numpad") resultsBox.textContent = resultsBox.textContent + " " + inputBox.textContent;
-    lastClass = "equals";
+    
     const resultsArr = resultsBox.textContent.split(" ")
                         .filter(x => x !== "");
     [num1, operation, num2] = [...resultsArr];
     resultsBox.textContent = operations[operation](parseFloat(num1), parseFloat(num2));
-    if(fromButton) inputBox.textContent = "";
-
+    if(fromButton) {
+        inputBox.textContent = "";
+        curClass = "equals";
+    } else { lastClass = "equals" }
 }
 
 function clearLast() {
